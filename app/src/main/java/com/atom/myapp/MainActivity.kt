9465a91ea9510ae510f462c24cf11dc16faaea0f
khwarenah <kikefishersindiana@gmail.com>
2026-09-14 17:27:58 -0600

    Practica 3

--- app/src/main/java/com/atom/myapp/MainActivity.kt
+++ app/src/main/java/com/atom/myapp/MainActivity.kt
@@ -17,23 +17,35 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.atom.myapp.ui.theme.MyAppTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -87,7 +99,18 @@ fun Formulario(modifier: Modifier = Modifier) {
                     .width(110.dp)
                     .height(45.dp)
             ) {
-                Text(text = "Sumar", fontSize = 16.sp, color = Color.Magenta)
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Sumar",
+                    tint = Color.Magenta,
+                    modifier = Modifier.size(20.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "",
+                    fontSize = 16.sp,
+                    color = Color.Magenta
+                )
             }
 
             Spacer(modifier = Modifier.height(10.dp))
@@ -100,7 +123,18 @@ fun Formulario(modifier: Modifier = Modifier) {
                     .width(110.dp)
                     .height(45.dp)
             ) {
-                Text(text = "Restar", fontSize = 16.sp, color = Color.Magenta)
+                Icon(
+                    imageVector = Icons.Filled.Remove,
+                    contentDescription = "Restar",
+                    tint = Color.Magenta,
+                    modifier = Modifier.size(20.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "",
+                    fontSize = 16.sp,
+                    color = Color.Magenta
+                )
             }
         }
 
@@ -152,13 +186,22 @@ fun Formulario(modifier: Modifier = Modifier) {
                 colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
                 shape = RoundedCornerShape(20.dp),
                 modifier = Modifier
-                    .width(110.dp)
+                    .width(140.dp)
                     .height(45.dp)
             ) {
-                Text(text = "Sumar", fontSize = 16.sp, color = Color.Magenta)
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Sumar",
+                    tint = Color.Magenta,
+                    modifier = Modifier.size(20.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "",
+                    fontSize = 16.sp,
+                    color = Color.Magenta
+                )
             }
-
-            Spacer(modifier = Modifier.height(10.dp))
 
             Button(
                 onClick = { contadorDerecha-- },
@@ -168,7 +211,20 @@ fun Formulario(modifier: Modifier = Modifier) {
                     .width(110.dp)
                     .height(45.dp)
             ) {
-                Text(text = "Restar", fontSize = 16.sp, color = Color.Magenta)
+
+                Icon(
+                    imageVector = Icons.Filled.Remove,
+                    contentDescription = "Restar",
+                    tint = Color.Magenta,
+                    modifier = Modifier.size(20.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "",
+                    fontSize = 16.sp,
+                    color = Color.Magenta
+                )
+
             }
         }
 
